bbedd2332d15ff96fecfbff3c88c27c40dd60382
Jister <chenjiesjtu14@gmail.com>
2016-11-18 20:29:41 +0800

    beta

--- src/tf20_node.cpp
+++ src/tf20_node.cpp
@@ -1,5 +1,6 @@
 #include "ros/ros.h"
 #include "std_msgs/Float32.h"
+#include "de_lidar/Lidar.h"
 #include <math.h>
 
 #include <stdio.h>
@@ -12,43 +13,76 @@
 #include <errno.h>
 
 #define BAUDRATE 	460800
-#define DEVICE 	"/dev/ttyUSB0"  
-#define MAXSIZE 50
+#define MAXSIZE 	50
+#define BUF_SIZE 	24
 
 int ret;
 int serial_fd;
-int direction;
-double velocity;
-char read_buf[24];
+std::string device;
+char read_buf[BUF_SIZE];
 char ring_buf[MAXSIZE];
+char data_buf[24];
 int read_addr = 0;
 int write_addr = 0;
 
+bool read_valid = false;
+bool crc_valid = false;
+
+unsigned long crc_data;
+unsigned long crc_recieved;
+unsigned long crc_buf[5];
+
+de_lidar::Lidar msg;
+float distance = 0;
+float amplitude = 0; 	//0-2500
+
 int set_serial(int fd, int nSpeed, int nBits, char nEvent, int nStop) ;
 void serial_init();
-int read();
 int next_data_handle(int addr);
-void write_data(char data);//put the data into the ringbuffer;
-void read_data();//read the data from ringbuffer to the cmd and velocity;
+int next_data_handle(int addr , int count) ;
+void write_data(char data);
+void read_data();
+unsigned long crc32gen(unsigned long data[], unsigned long size);
 
 int main(int argc, char **argv)
 {
 	ros::init(argc, argv, "tf20_node");
 	ros::NodeHandle n;
-    ros::Publisher lidar_pub = n.advertise<std_msgs::Float32>("/delidar", 10);
+    ros::Publisher lidar_pub = n.advertise<de_lidar::Lidar>("/lidar", 10);
 	ros::Rate loop_rate(50);
+
+	if (n.getParam("device", device))
+    {
+    	ROS_INFO("Device set to: %s", device.c_str());
+    }
+    else
+    {
+    	device = "/dev/ttyUSB0";  
+    	ROS_INFO("Using the default USB device: /dev/ttyUSB0");
+    }
 
 	serial_init();
 	while(ros::ok())
 	{
+		read_data();
+		ROS_INFO("crc calculated:%lu",crc_data);
+		ROS_INFO("crc crc_recieved:%lu", crc_recieved);
+		if(read_valid && crc_valid)
+		{
+			msg.header.stamp = ros::Time::now();
+			msg.distance.data = distance;
+			msg.amplitude.data = amplitude;
+			lidar_pub.publish(msg);
 
+			read_valid = false;
+			crc_valid = false;
+		}
 		ros::spinOnce();
 		loop_rate.sleep();
 	}
 	close(serial_fd);
 	return 0;
 }
-
 
 int set_serial(int fd,int nSpeed, int nBits, char nEvent, int nStop)  
 {  
@@ -152,7 +186,7 @@ int set_serial(int fd,int nSpeed, int nBits, char nEvent, int nStop)
 void serial_init()
 {
 	//serial_fd = open(DEVICE, O_RDWR | O_NOCTTY | O_NONBLOCK);
-	serial_fd = open(DEVICE, O_RDWR | O_NOCTTY);
+	serial_fd = open(device.c_str(), O_RDWR | O_NOCTTY);
 	if (serial_fd == -1)
 	{
 		ROS_INFO("Open Error!");  
@@ -170,6 +204,17 @@ int next_data_handle(int addr) {
 	return (addr + 1) == MAXSIZE ? 0 : (addr + 1);
 }
 
+int next_data_handle(int addr , int count)     
+{     
+  	int a;
+  	a = addr;
+  	for(int i = 0; i < count ; i++)
+  	{ 
+    		a = ( (a + 1)  == MAXSIZE ?  0 : ( a + 1 ) ) ;   
+  	}
+  	return a;  
+}
+
 void write_data(char data)
 {
 	*(ring_buf+write_addr) = data;
@@ -178,50 +223,76 @@ void write_data(char data)
 
 void read_data()
 {
-	if (ring_buf[read_addr] == 'M')
+	memset(read_buf, 0, BUF_SIZE);
+	ret = read(serial_fd, read_buf, BUF_SIZE);
+	if( ret <= 0 )
 	{
-		direction = ring_buf[read_addr + 1]-48;
-        velocity = (ring_buf[read_addr + 4] - 48) / 300.0;
-		for (int i = 0; i < 6; i++) {
-			read_addr = next_data_handle(read_addr);
+		ROS_INFO("read err: %d\n", ret);
+
+	}else
+	{
+		for(int i = 0 ; i < BUF_SIZE ; i++)
+		{
+			write_data(read_buf[i]);
 		}
 	}
-	else
+
+	for(int i = 0 ; i < MAXSIZE ; i++)
 	{
-		for (int i = 0; i < 6; i++)
+		if((ring_buf[read_addr] == 0x44) && (ring_buf[next_data_handle(read_addr)] == 0x45) 
+			&& (ring_buf[next_data_handle(read_addr,2)] == 0x32) && (ring_buf[next_data_handle(read_addr,3)] == 0x30))  
 		{
-			read_addr = next_data_handle(read_addr);
-			if (ring_buf[read_addr] == 'M')
+			for(int j = 0 ; j < 24 ; j++)
 			{
-				break;
+				data_buf[j] = ring_buf[read_addr] ;
+				read_addr = next_data_handle(read_addr) ;
 			}
+			read_valid = true;
+			break;
+		}else
+		{
+			read_addr = next_data_handle(read_addr) ;
 		}
+	}
+
+	for(int i = 0; i < 5 ; i++)
+	{
+		crc_buf[i] = ((unsigned long)data_buf[4*i+3]<<24) | ((unsigned long)data_buf[4*i+2]<<16) | ((unsigned long)data_buf[4*i+1]<<8) | ((unsigned long)data_buf[4*i+0]);
+	}
+	crc_data = crc32gen(crc_buf, 5);
+	crc_recieved = ((unsigned long)data_buf[23]<<24) | ((unsigned long)data_buf[22]<<16) | ((unsigned long)data_buf[21]<<8) | ((unsigned long)data_buf[20]) ;
+
+	if(crc_data == crc_recieved)
+	{
+		crc_valid = true;
+	}
+
+	if(read_valid && crc_valid)
+	{
+		distance = ((short)data_buf[9]<<8) | ((short)data_buf[8]) ;
+		amplitude = ((short)data_buf[13]<<8) | ((short)data_buf[12]) ;
+		
 	}
 }
 
-int read()
+unsigned long crc32gen(unsigned long data[], unsigned long size)
 {
-	int ret;
+	unsigned long temp, crc = 0xFFFFFFFF;
+	for(int i = 0; i < size; i++)
+	{
+		temp = data[i];
+		for(int j = 0; j < 32; j++)
+		{
+			if((crc ^ temp) & 0x80000000)
+			{
+				crc = 0x04C11DB7 ^ (crc << 1);
+			}else
+			{
+				crc <<= 1;
+			}
 
-	memset(read_buf, 0, 50);
-	ret = read(serial_fd, read_buf, 6);
-	if (ret < 0)
-	{
-		ROS_INFO("read error!");
-		return -1;
+			temp <<= 1;
+		}
 	}
-	else if (ret == 0)
-	{
-		//ROS_INFO("No data!");
-		return 0;
-	}
-	else{
-        /*ROS_INFO("data:%c",read_buf[0]);
-		ROS_INFO("data:%c",read_buf[1]);
-		ROS_INFO("data:%c",read_buf[2]);
-		ROS_INFO("data:%c",read_buf[3]);
-		ROS_INFO("data:%c",read_buf[4]);
-        ROS_INFO("data:%c",read_buf[5]);
-        return ret;*/
-	}
+	return crc;
 }
